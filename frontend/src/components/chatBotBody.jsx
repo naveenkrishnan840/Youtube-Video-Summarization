fd0675f1f7439d375aa070a63d19b6bec909ece5
NavaneethanJeyapraka <naveenkrishnan840@gmail.com>
2025-02-10 17:50:55 +0530

    frontend code committed.

--- frontend/src/components/chatBotBody.jsx
+++ frontend/src/components/chatBotBody.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
-import {Input, TextareaAutosize} from "@mui/material";
+import React, { use, useRef, useState, useEffect } from "react";
+import {Fab, Input, TextareaAutosize} from "@mui/material";
 import "react-chat-elements/dist/main.css"
 import {Formik, Form, Field} from "formik";
-import {RequestService} from "./request";
+import {RequestService, RequestUrlService} from "./request";
 import parse from 'html-react-parser';
 import * as yup from "yup";
-
+import {MessageList, Avatar} from "react-chat-elements";
+import UploadOutlinedIcon from '@mui/icons-material/UploadOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Alert from '@mui/material/Alert';
+import { Button, CircularProgress } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import Backdrop from '@mui/material/Backdrop';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const formValidationSchema = yup.object().shape({
     inputmessage: yup.string().required("Messgaes is Required"),
@@ -13,72 +24,194 @@ const formValidationSchema = yup.object().shape({
 });
 
 export default function ChatBotBody () {
-    const [messages, setMessages] = useState("");
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 650,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+    const [messages, setMessages] = useState([]);
+    const [url, setUrl] = useState("");
+    const [embedUrl, setembedUrl] = useState("");
+    const [videoProcessed, setVideoProcessed] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const heightRef = useRef(null);
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const updateHeight = () => {
+        if (heightRef.current) {
+          const newHeight = heightRef.current.scrollHeight;
+          heightRef.current.style.height = `${newHeight}px`;
+        }
+      };
     
-        const onSubmitForm = (values) => {
-        const data = {"input_msg": values["inputmessage"]}
-        const response = RequestService("/bot-message-request", data);
+      // Recalculate the height whenever messages change
+      useEffect(() => {
+        updateHeight();
+      }, [messages]);
+    const submitVideoId = () => {
+        setIsLoading(true)
+        const response = RequestUrlService("/videoUrlProcessing", {"url": url});
         response.then((res)=>{
-            // if (res.detail){
-            //     setMessages((prevMsg)=>
-            //         [...prevMsg, {
-            //             position: "left",
-            //             type: "text",
-            //             title: "Bot",
-            //             text: parse(res.detail),
-            //             // styles: {backgroundColor:"gray"},
-            //             className: "text-black",
-            //             date: new Date()
-            //         }]
-            //     )
-            // }
-        })
+            if (res.detail.status == true){
+                setMessages((prevMsg)=>
+                    [...prevMsg, {
+                        position: "left",
+                        type: "text",
+                        title: "Bot",
+                        text: "Hi, How can i help you! I can assist you!",
+                        focus: true,
+                        className: "text-black max-h-screen",
+                        date: new Date()
+                    }])
+                setembedUrl(`https://www.youtube.com/embed/${url.split('?')[1]}`)
+                setVideoProcessed(true)
+                setIsDisabled(true)
+                setIsError(false)
+            } else {
+                setIsError(true)
+            }
+            setIsLoading(false)
+        });
     }
+    const uploadVideo = () => {
+        setVideoProcessed(false)
+        setIsDisabled(false)
+    }
+    const onSubmitForm = async (values) => {
+        
+        const data = {"query": values["query"]};
+        setMessages((prevMsg)=>
+            [...prevMsg, {
+                position: "right",
+                type: "text",
+                title: "User",
+                text: values["query"],
+                // styles: {backgroundColor:"gray"},
+                className: "text-black max-h-screen",
+                date: new Date()
+            }]
+        )
+        try {
+          const response = await fetch('http://127.0.0.1:8006/query', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data) ,
+          });
+    
+          const reader = response.body?.getReader();
+          if (!reader) return;
+    
+          const decoder = new TextDecoder();
+          while (true) {
+            const { done, value } = await reader.read();
+            if (done) break;
+    
+            let streamingMessage = decoder.decode(value, { stream: true });
+            setMessages((prevMsg)=>
+                [...prevMsg, {
+                    position: "left",
+                    type: "text",
+                    title: "Bot",
+                    text: streamingMessage,
+                    // styles: {backgroundColor:"gray"},
+                    className: "text-black",
+                    date: new Date()
+                }]
+            )
+          }
+        } catch (error) {
+          console.error('Failed to send query:', error);
+          setMessages(prev => [...prev, {
+            type: 'error',
+            content: 'Failed to connect to the server'
+          }]);
+        } finally {
+          setIsLoading(false);
+        }
+      };
     return (
         <>
-            <div className="relative ">
-                <div className="w-auto h-14 my-1 shadow-md rounded-md p-2 text-wrap text-center font-mono font-extrabold size-4 text-lg text-white">
+            {isError && <Alert onClose={()=>{setIsError(false)}} severity="error">Youtube Url is not valid. Can you check the url</Alert>}
+            <div className="h-auto w-auto">
+                {videoProcessed && <div className="w-auto h-14 my-1 shadow-md rounded-md p-2 text-wrap text-center font-mono font-extrabold size-4 text-lg text-white">
                     YouTube Video Summarization
-                </div>
-                <div className="flex flex-col mx-40 rounded-md overflow-y-auto text-white shadow-lg" style={{height: "380px"}}>
-                    {/* <MessageList lockable={true} className="h-80 overflow-y-auto overflow-hidden " dataSource={messages} /> */}
-                </div>
+                </div>}
+                {videoProcessed ? 
+                <>
+                <div className="flex flex-col mx-40 rounded-md overflow-y-auto text-white shadow-lg" style={{height: "380px", width: "980px"}}>
+                    <div className="h-80 overflow-y-auto overflow-hidden">
+                    <MessageList referance={heightRef} dataSource={messages}/>  
+                    </div>
+                </div> 
+                </>:
+                <div className="relative top-16 mx-40 my-20 h-56 text-wrap text-center font-mono font-extrabold text-2xl text-white">
+                    YouTube Video Summarization
+                    <div className="text-xl">
+                     What can I help with?
+                    </div>
+                </div>}
+                
                 <Formik
                     validateOnBlur={false}
                     validateOnChange={false}
-                        initialValues={{ inputmessage: ""}}
+                        initialValues={{ query: ""}}
                         onSubmit={async (values, { resetForm }) => {
-                            // setMessages((prevMsg)=>
-                            //     [...prevMsg, {
-                            //         position: "right",
-                            //         type: "text",
-                            //         title: "User",
-                            //         text: values.inputmessage,
-                            //         className: "text-black",
-                            //         // styles: {"color":"#2f2f2f"},
-                            //         date: new Date()
-                            //     }]
-                            // )
                             onSubmitForm(values)
                             resetForm({
-                                values: { inputmessage: "",  youTubeUrl: ""}
+                                values: { query: ""}
                             })
                         }}
-                        validationSchema={formValidationSchema}
+                        // validationSchema={formValidationSchema}
                     >
                         {({ setFieldValue, values, submitForm, errors }) => (
                             <Form>
-                                <div className="grid grid-flow-row gap-y-2">
+                                <div className="grid grid-flow-row gap-y-3">
                                     <div className="mx-40 h-10 px-2">
                                         <Field name="inputmessage" >
                                         {({ field }) => (
                                             <>
-                                                <Input  required placeholder="Enter the YouTube Url..." sx={{paddingLeft: "10px"}} {...field} className="focus:outline-none block h-10 w-full resize-none rounded-md border-0 px-0 py-2 bg-slate-200" autoFocus=""></Input>
+                                                <div className="flex flex-row">
+                                                    <Input disabled={isDisabled} onChange={(e)=> setUrl(e.target.value)} required placeholder="Enter the YouTube Url..." sx={{paddingLeft: "10px", width: "900px"}} value={url} className="focus:outline-none block h-15 resize-none rounded-md border-0 px-0 py-2 bg-slate-200" autoFocus=""></Input>
+                                                    {!isLoading ? 
+                                                        <Tooltip title="Upload the url">
+                                                            <IconButton disabled={isDisabled} sx={{color:"red", height: "32px", width: "35px"}} className="relative border-1 bg-white rounded-lg float-right right-9 top-2 cursor-pointer">
+                                                                <UploadOutlinedIcon onClick={submitVideoId}/> 
+                                                            </IconButton>
+                                                        </Tooltip>
+                                                    : 
+                                                    <Button className="relative bottom-10 left-3 float-right" 
+                                                        size="medium"
+                                                        startIcon={
+                                                            isLoading ? (
+                                                              <CircularProgress size={25} sx={{ color: 'red' }} />  // Change color here
+                                                            ) : null
+                                                          }
+                                                    />
+                                                    }
+                                                    <Tooltip title="Add url">
+                                                        <IconButton sx={{color:"black", height: "32px", width: "35px"}} className="relative right-8 top-2 border-2 bg-white rounded-lg size-4 cursor-pointer">
+                                                            <AddIcon onClick={uploadVideo}/> 
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </div>
+                                                
                                             </>
                                         )}
                                         </Field>
                                     </div>
-                                    <Field name="youTubeUrl">
+                                    <Field name="query">
                                         {({ field }) => (
                                             <>
                                                 <div className="group flex mx-40">
@@ -101,10 +234,29 @@ export default function ChatBotBody () {
                                                                 </div>
                                                             </div>
                                                         </div>
+                                                        {videoProcessed && <Button  className="hover:bg-black" sx={{position: "relative", left: "450px" , bottom: "45px"}} onClick={handleOpen} color="info">Open Video</Button>}
+                                                        <Modal
+                                                            aria-labelledby="transition-modal-title"
+                                                            aria-describedby="transition-modal-description"
+                                                            open={open}
+                                                            onClose={handleClose}
+                                                            closeAfterTransition
+                                                            slots={{ backdrop: Backdrop }}
+                                                            slotProps={{
+                                                            backdrop: {
+                                                                timeout: 500,
+                                                            },
+                                                            }}
+                                                        >
+                                                            
+                                                            <Box sx={style}>
+                                                                <iframe width="560" height="315" src={embedUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                                            </Box>
+                                                            </Modal>
                                                         <span data-state="closed">
-                                                            <button onSubmit={submitForm} disabled="" aria-label="Send prompt" data-testid="send-button" className="flex h-8 w-8 items-center justify-center rounded-full transition-colors hover:opacity-70 focus-visible:outline-none focus-visible:outline-black disabled:text-[#f4f4f4] disabled:hover:opacity-100 dark:focus-visible:outline-white disabled:dark:bg-token-text-quaternary dark:disabled:text-token-main-surface-secondary bg-black text-white dark:bg-white dark:text-black disabled:bg-[#D7D7D7]">
+                                                            {videoProcessed && <button type="submit" onSubmit={submitForm} disabled="" aria-label="Send prompt" data-testid="send-button" className="flex h-8 w-8 items-center justify-center rounded-full transition-colors hover:opacity-70 focus-visible:outline-none focus-visible:outline-black disabled:text-[#f4f4f4] disabled:hover:opacity-100 dark:focus-visible:outline-white disabled:dark:bg-token-text-quaternary dark:disabled:text-token-main-surface-secondary bg-black text-white dark:bg-white dark:text-black disabled:bg-[#D7D7D7]">
                                                                 <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className="icon-2xl"><path fillRule="evenodd" clipRule="evenodd" d="M15.1918 8.90615C15.6381 8.45983 16.3618 8.45983 16.8081 8.90615L21.9509 14.049C22.3972 14.4953 22.3972 15.2189 21.9509 15.6652C21.5046 16.1116 20.781 16.1116 20.3347 15.6652L17.1428 12.4734V22.2857C17.1428 22.9169 16.6311 23.4286 15.9999 23.4286C15.3688 23.4286 14.8571 22.9169 14.8571 22.2857V12.4734L11.6652 15.6652C11.2189 16.1116 10.4953 16.1116 10.049 15.6652C9.60265 15.2189 9.60265 14.4953 10.049 14.049L15.1918 8.90615Z" fill="currentColor"></path></svg>
-                                                            </button>
+                                                            </button>}
                                                         </span>
                                                     </div>
                                                     </div>
--- frontend/src/components/request.js
+++ frontend/src/components/request.js
@@ -7,7 +7,24 @@ export const RequestService = (path, data) => {
         header_data["Authorization"] = `Bearer ${localStorage.getItem("token")}`
     }
     var body = JSON.stringify(data) 
-    var path = `http://127.0.0.1:8004${path}`
+    var path = `http://127.0.0.1:8006${path}`
+    return fetch(path, {
+        method: "POST",
+        headers: header_data,
+        body: body
+    }).then(res => res.json())
+}
+
+
+export const RequestUrlService = (path, data) => {
+    const header_data = {
+        'Content-Type': 'application/json'
+    }
+    if (localStorage.getItem("token")){
+        header_data["Authorization"] = `Bearer ${localStorage.getItem("token")}`
+    }
+    var body = JSON.stringify(data) 
+    var path = `http://127.0.0.1:8006${path}`
     return fetch(path, {
         method: "POST",
         headers: header_data,
